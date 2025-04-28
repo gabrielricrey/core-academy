66047528ab7d88aa8c801c4cc98a97da17b2b942
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-28 14:08:40 +0200

    change import on auth

--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Course = require("../models/Course");
-const auth = require("../middleware/auth");
+const {auth} = require("../middleware/auth");
 
 // Get all courses
 router.get("/", async (req, res) => {
