cb7907fec883718fe710696ef754a5b5e3bd1dd4
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-28 14:06:56 +0200

    add adminauth and change error message to 404

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,23 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
+const auth = (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(401).json({ message: "Authentication required" });
+    res.status(404).json({ message: "Not found" });
   }
 };
+
+const adminAuth = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(404).json({ message: "Not Found" });
+  }
+
+}
+
+module.exports = { auth, adminAuth };
